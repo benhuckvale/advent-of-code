bfca64e2ed77e014db91d44a945f3150492740a7
Ben Huckvale <5511911+benhuckvale@users.noreply.github.com>
2024-12-02 11:30:21 +0000

    2024 Day 1: Calculate similarity also

--- 2024/1/sorted_list_distance.swift
+++ 2024/1/sorted_list_distance.swift
@@ -30,14 +30,27 @@ func main() {
     let filename = CommandLine.arguments[1]
     var (column1, column2) = parseFile(filename: filename)
     
+    // Part 1
     column1.sort()
     column2.sort()
 
-    let result = zip(column1, column2)
+    let distance = zip(column1, column2)
         .map { abs($0 - $1) }
         .reduce(0, +)
 
-    print(result)
+    // Part 2
+    let column2_occurrences_for_value = column2.reduce(into: [Int: Int]()) { counts, value in
+        counts[value, default: 0] += 1
+    }
+
+    let similarity = column1
+        // multiple each value in column 1 by the number of occurences in column 2
+        .map { value in column2_occurrences_for_value[value, default: 0] * value }
+        // Sum the result
+        .reduce(0, +)
+
+    print("Distance: ", distance)
+    print("Similarity: ", similarity)
 }
 
 main()
